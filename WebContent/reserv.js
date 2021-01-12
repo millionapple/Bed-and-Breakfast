ad1767d497665e446225be3b5e17b5bbde7057ce
GarrettBentley <garrettrussell1999@gmail.com>
2021-01-11 22:09:08 -0600

    connected the MadeReservations page to the servlet and have it doGet on load.

---
+++ WebContent/reserv.js
@@ -0,0 +1,10 @@
+function getReservations() {
+		  var xhttp = new XMLHttpRequest();
+		  xhttp.onreadystatechange = function() {
+		    if (this.readyState == 4 && this.status == 200) {
+		      document.getElementById("demo").innerHTML = this.responseText;
+		    }
+		  };
+		  xhttp.open("GET", "Reservations", true);
+		  xhttp.send();
+		}
